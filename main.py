ee93b8b5d2be6ae923b7dee279b525ce7e886281
nthumu <44855085+nthumu@users.noreply.github.com>
2022-02-14 17:59:44 +0530

    test

--- main.py
+++ main.py
@@ -6,3 +6,20 @@
 # Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("Input/Names/invited_names.txt") as invitees:
+    names = invitees.readlines()
+    print(names)
+
+with open("Input/Letters/starting_letter.txt") as letter_sample:
+    letter = letter_sample.readlines()
+    for line in letter:
+        print(line)
+
+for name in names:
+    with open(f"Output/ReadyToSend/letter_for_{name}.txt", "w") as final_letter:
+        for line in letter:
+            line.replace("[name]", str(name))
+            final_letter.write(line)
+
+
